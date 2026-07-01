43b84fa0e9b1bfc510447c48aca90b5508185825
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-07-01 15:12:01 +0100

    Handle but throw away additional args in _handle_serverstatus (#528)
    * Handle but throw away additional args
    
    https://lyrion.org/reference/cli/compoundqueries/#serverstatus
    
    https://lyrion.org/reference/cli/using-the-cli/#extended-query-format
    
    * Tweak wording

--- aioslimproto/cli.py
+++ aioslimproto/cli.py
@@ -721,7 +721,7 @@ class SlimProtoCLI:
         player_id: str,
         start_index: int | str = 0,
         limit: int = 999,
-        *args,  # Added to gracefully handle non spec compliant devices
+        *args,  # Accept but discard any additional args
         **kwargs,
     ) -> PlayersResponse:
         """Handle players command."""
@@ -848,6 +848,7 @@ class SlimProtoCLI:
         player_id: str,
         start_index: int = 0,
         limit: int = 2,
+        *args,  # Accept but discard any additional args
         **kwargs,
     ) -> ServerStatusResponse:
         """Handle server status command."""
