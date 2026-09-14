5515fbf182744e64414efba57bfa689b989ba586
Jeff lawrence <jefflawrence@Jeffs-Mac-mini.lan>
2026-09-14 00:54:52 -0500

    Add support for - as a limit not just a start index

--- aioslimproto/cli.py
+++ aioslimproto/cli.py
@@ -955,8 +955,11 @@ class SlimProtoCLI:
         **kwargs,
     ) -> ServerStatusResponse:
         """Handle server status command."""
+        #Devices sometimes send ['serverstatus', '-', '-', []]]
         if start_index == "-":
             start_index = 0
+        if limit == "-": 
+            limit = float("inf")
         players: list[PlayerItem] = []
         for index, player in enumerate(self.server.players):
             if isinstance(start_index, int) and index < start_index:
